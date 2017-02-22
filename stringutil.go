9b93b1b2a846cf7715a4cb91ce2f0ead86265b98
Jamie Sinn <james.sinn@sinndevelopment.com>
2017-02-22 00:48:30 -0500

    Update stringutil.go

--- stringutil.go
+++ stringutil.go
@@ -8,9 +8,3 @@ func GetFinalArgs(slice []string, start int) string {
 	result := strings.Join(slice[start:len(slice)], " ")
 	return result
 }
-
-func Check(e error) {
-    if e != nil {
-        panic(e)
-    }
-}
